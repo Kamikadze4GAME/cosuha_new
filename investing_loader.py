a850abd57b0d757a553ec25cbfda75f3cc050160
Mishchenko Mykhailo <@irritateddog>
2018-10-20 17:12:47 +0300

    Dataset loader finished.

--- investing_loader.py
+++ investing_loader.py
@@ -1,89 +1,78 @@
 
 from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
-from pandas import DataFrame
-import re
-from datetime import datetime
+from pandas import DataFrame, DatetimeIndex
+import json
+import time
 
-req = Request('https://www.investing.com/crypto/'
-              'currency-pairs?c1=189&c2=12',
+def load_prices_history(symbol=None, resolution=None, _from=None, to=None):
+    
+    if symbol is None:
+        symbol=945629  #bitcoin
+    if resolution is None:
+        resolution=15
+    
+    if to is None:
+        to = int(time.time())
+    
+    if _from is None:
+        _from = to - 3600 * 24 * 14
+    
+    hist_req = Request('https://tvc4.forexpros.com/'
+                       '6a8e228b53365fe58a6196efc7b1f3ee/'
+                       '1540024500/1/1/8/history?'
+                       'symbol={symbol}&resolution={resolution}&'
+                       'from={_from}&to={to}'.format(
+                            symbol=symbol,
+                            resolution=resolution,
+                            _from=_from,
+                            to=to
+                       ),
               headers={'User-Agent':'Mozilla/5.0 '
                                     '(X11; Linux x86_64; rv:62.0) '
                                     'Gecko/20100101 Firefox/62.0'
                       }
              )
+    
+    jstr = urlopen(hist_req).readall().decode('utf-8')
+    return json.loads(jstr)
 
-def load_prices_page():
-    return urlopen(req).read()
+def prices_to_DataFrame(j):
+    if j['s'] != 'ok':
+        raise Exception('bad history; status: {}'.format(j['s']))
+    
+    return DataFrame(
+        index=DatetimeIndex([t*10**9 for t in j['t']]),
+        data={  'Last':j['l'],
+                'Open':j['o'],
+                'High':j['h'],
+                'Low' :j['l'],
+                'Vol.':j['vo']
+        }
+    )
 
-class Prices:
-    def __init__(self, page=None, date_time=None):
-        
-        if date_time is None or page is None:
-            date_time = datetime.now()
-        if page is None:
-            page = load_prices_page()
-        
-        self.date_time = date_time
-        
-        self.page = page
-        html_table = BeautifulSoup(page)\
-                    .find('table', id='crypto_currencies_189')
-        
-        head = html_table.find('thead')
-        columns = [ tag.text for tag in head('th')[1:-1] ]
-        ex_name_columns = list(filter(lambda i: columns[i] == 'Exchange', range(len(columns))))
-        if len(ex_name_columns) != 1:
-            raise Exception("Bad number of columns with exchange's name.",
-                            ex_name_columns)
-        ex_name_column = ex_name_columns[0]
-        del ex_name_columns
-        del columns[ex_name_column]
-        
-        def remove_spaces(s):
-            s = re.sub(r'^\s+', '', s)
-            s = re.sub(r'\s+$', '', s)
-            return s
-        columns = [remove_spaces(s) for s in columns]
-        
-        rows  = []
-        row_index = []
-        for row in html_table.find('tbody')('tr'):
-            fields = row('td')
-            row_index.append(fields[ex_name_column+1].text)
-            rows.append([f.text for f in fields[1:ex_name_column+1] + fields[ex_name_column+2:-1]])
-        
-        self.table = DataFrame(data=rows, columns=columns, index=row_index, dtype=str)
+if __name__ == '__main__':
+    import argparse as ap
+    parser = ap.ArgumentParser()
+    parser.add_argument('--from', '-f', type=int, dest='_from', help='beginning time')
+    parser.add_argument('--to', '-t', type=int, help='end time')
+    parser.add_argument('--symbol', '-s', type=int, help='time serie (symbol) id')
+    parser.add_argument('--resolution', '-r', type=int, help='resolution in minutes')
+    parser.add_argument('fname', nargs='?', help='output fname')
+    args = parser.parse_args()
     
-    @classmethod
-    def vol_conv(s):
-        m = re.match(r'([\d.]+)\s*([KkMmGg])\s*$', s)
-        num = float(m.group(1))
-        exp = m.group(2).lower()
-        
-        if exp == '':
-            pass
-        elif exp == 'k':
-            num *= 1000
-        elif exp == 'm':
-            num *= 1000000
-        elif exp == 'g':
-            num *= 1000000000
-        
-        return num
+    res = args.resolution if args.resolution else 15
     
-    @classmethod
-    def val_conv(s):
-        return float(s.relace(',','')
-    
-    def time_conv(self, s):
-        m = re.match(r'(?P<h>\d\d):(?P<m>\d\d):(?P<s>\d\d)', s)
+    hist = load_prices_history(_from=args._from, to=args.to,
+                               symbol=args.symbol,
+                               resolution=res)
     
     
-    def __getitem__(self, key):
-        serie = self.table.loc[key]
-        
-        vol = self.vol_conv(serie['Vol.'])
-
-
-
+    if args.fname:
+        fname = args.fname
+    else:
+        fname = '{} - {} interval {}m.json'.format(
+                    hist['t'][0],
+                    hist['t'][-1],
+                    res)
+    with open(fname, 'w') as f:
+        json.dump(hist, f)
